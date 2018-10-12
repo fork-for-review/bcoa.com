afbc8fe7c0e7db1a7af210cb6d207cecbb01b3a3
joshuarule <joshua@joshuarule.com>
2018-10-12 14:00:27 -0400

    adds analytics again

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,26 @@ module.exports = {
     "gatsby-transformer-json",
     'gatsby-plugin-react-next',
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-61023410-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: [],
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        // cookieDomain: "example.com",
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/static/images/uploads`,
